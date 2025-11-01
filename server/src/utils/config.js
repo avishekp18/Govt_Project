d7ca86910252936539aba34f4aed5f0a2a601421
avishekp18 <avishekpradhan24@gmail.com>
2025-11-01 15:25:10 +0530

    updated all

--- server/src/utils/config.js
+++ server/src/utils/config.js
@@ -1,9 +1,19 @@
-require("dotenv").config({ path: process.cwd() + "/.env" }); // Ensure .env is loaded from root
+const fs = require("fs");
+const path = require("path");
+
+// Load .env only if it exists (e.g., in local dev)
+const envPath = path.join(process.cwd(), ".env");
+if (fs.existsSync(envPath)) {
+  require("dotenv").config({ path: envPath });
+}
 
 const config = {
   port: process.env.PORT || 5000,
   mongoURI: process.env.MONGO_URI,
-  corsOrigin: process.env.CORS_ORIGIN || "http://localhost:3000",
+  corsOrigin:
+    process.env.CORS_ORIGIN ||
+    "http://localhost:3000" ||
+    "https://govt-project-six.vercel.app",
   dataGovApiKey: process.env.DATA_GOV_API_KEY,
   geocoder: {
     provider: process.env.GEOCODER_PROVIDER,
